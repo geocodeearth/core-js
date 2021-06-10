3a7f4e1c4fb1c8ed8ead8c0e2cfac47c6dccb0d2
Max Lielje <max@maxlielje.co>
2021-06-10 10:49:46 +0200

    fix: url: set default host correctly

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,11 +3,12 @@ import { Options, defaultOptions } from '../options'
 
 type Endpoint = 'autocomplete'
 
-export const createURL = (endpoint: Endpoint, query: Query, { host, client }: Options = defaultOptions): URL => {
+export const createURL = (endpoint: Endpoint, query: Query, options: Options): URL => {
+  const host = options.host === undefined ? defaultOptions.host : options.host
   const url = new URL(`/v1/${endpoint}`, `https://${host}`)
 
-  if (client !== undefined) {
-    query.client = client
+  if (options.client !== undefined) {
+    query.client = options.client
   }
 
   url.search = new URLSearchParams(query).toString()
